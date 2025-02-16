c250842956c7d0373ea66c0b58f8c4fa45cd4da9
Akanimo Sam Okure <akan.okure@gmail.com>
2025-02-16 08:03:10 +0000

    open cart in modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,18 +1,20 @@
 import { createPortal } from "react-dom";
 import { useEffect, useRef } from "react";
 
-export default function Modal(children, open, className = "") {
+export default function Modal({ children, open, className = "" }) {
   const dialog = useRef();
 
-  useEffect(() => {}, [open]);
+  useEffect(() => {
+    const modal = dialog.current;
+    if (open) {
+      modal.showModal();
+    }
+    return () => modal.close();
+  }, [open]);
 
-  if (open) {
-    dialog.current.showModal();
-  }
-
-  createPortal(
+  return createPortal(
     <dialog ref={dialog} className={` modal ${className}`}>
-      children
+      {children}
     </dialog>,
     document.getElementById("modal")
   );

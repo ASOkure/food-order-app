d481cf77de222dae42c3339a1bbacfef09c6d7ff
Akanimo Sam Okure <akan.okure@gmail.com>
2025-02-17 18:06:24 +0000

    add custom input component and manage modal visibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 import { CartContextProvider } from "./store/CartContext";
 import { UserProgressContextProvider } from "./store/UserProgressContext";
+import Checkout from "./components/Checkout";
 function App() {
   return (
     <UserProgressContextProvider>
@@ -10,6 +12,7 @@ function App() {
         <Header />
         <Meals />
         <Cart />
+        <Checkout />
       </CartContextProvider>
     </UserProgressContextProvider>
   );
